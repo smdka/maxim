2fceace9b5f135625f471c7a5c3eb0e75b575bcb
smdka <Smdkaalaska@yandex.ru>
2023-11-29 17:43:03 +0300

    refactor: add json naming strategy (lower camel case) to RiderDto

--- rider-service/src/main/java/ru/golovachev/riderservice/dto/RiderDto.java
+++ rider-service/src/main/java/ru/golovachev/riderservice/dto/RiderDto.java
@@ -1,5 +1,7 @@
 package ru.golovachev.riderservice.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,6 +10,7 @@ import lombok.Value;
 
 @Value
 @Builder
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public class RiderDto {
     @NotBlank(message = "First name can't be null")
     String firstName;
