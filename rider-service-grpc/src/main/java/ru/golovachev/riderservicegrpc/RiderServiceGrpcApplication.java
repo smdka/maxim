41bc3bf98ee4c971bec65fa05f2115070f89dba8
smdka <Smdkaalaska@yandex.ru>
2023-12-12 11:45:48 +0300

    Initial commit

---
+++ rider-service-grpc/src/main/java/ru/golovachev/riderservicegrpc/RiderServiceGrpcApplication.java
@@ -0,0 +1,13 @@
+package ru.golovachev.riderservicegrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderServiceGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiderServiceGrpcApplication.class, args);
+    }
+
+}
---
+++ rider-service-grpc/src/test/java/ru/golovachev/riderservicegrpc/RiderServiceGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.golovachev.riderservicegrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderServiceGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
